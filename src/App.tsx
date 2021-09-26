c3be0af8b079b5e258caf37470a872609bfbb147
pritudev <contact.pritulx@gmail.com>
2021-09-26 15:10:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="My" color="coral"/>
+      <Header title="First Typescript" color="#007acc"/>
+      <Header title="App" color="coral"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+export interface Props {
+    title: string
+    color?: string
+}
+const Header = (props: Props) => {
+    return(
+        <header>
+            <h1 style={{color: props.color ? props.color : 'black'}}>{props.title}</h1>
+        </header>
+    )
+}
+
+export default Header
